9913199f6f8f6ed7696b5831079a8220794ea62e
Njoki <kokymash@gmail.com>
2025-02-20 19:52:54 +0300

    allfiles

---
+++ Frontend/src/pages/ManagmentTeam.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ManagementTeam = () => {
+  return (
+    <section className="management-team">
+      <h2>Management Team</h2>
+      <p>Zuruel, James, and Iris lead our organization.</p>
+    </section>
+  );
+};
+export default ManagementTeam;
