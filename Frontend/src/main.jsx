3658fbaa3de0d737e68ccecf779ae7049f58081e
Zuruel Kamande <zurueladrian@gmail.com>
2025-02-27 02:43:03 +0300

    removes id

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,8 +4,6 @@ import App from "./App";
 import "./index.css";
 
 
-const clientId = "YOUR_GOOGLE_CLIENT_ID"; // Replace with your actual client ID
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
       
